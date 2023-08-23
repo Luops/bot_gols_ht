78464e8909fed0437e7de35ed95a8a17789b4fef
fabricio rios <you@example.com>
2023-08-23 19:53:38 -0300

    Teste01

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
+# vercel-python=python3
+
 import requests
 import pandas as pd
 import numpy as np
@@ -151,5 +153,5 @@ def results():
   print("Loop concluído")
   return jsonify("Loop concluído"), 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+'''if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)'''
